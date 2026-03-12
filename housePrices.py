99ddd3938961b4c0709d08863c2d7435f5dbcbcf
tismajo <mariajosegironisidro@gmail.com>
2026-03-11 19:05:00 -0600

    Carga del dataset y exploración básica de estructura, Análisis exploratorio inicial

--- housePrices.py
+++ housePrices.py
@@ -9,9 +9,35 @@ from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
+"""## 2. Cargar el dataset"""
+
 df = pd.read_csv('train.csv')
 df.head()
+
+"""### Información general del dataset"""
 
 df.info()
 
 df.describe()
+
+"""## 3. Análisis Exploratorio de Datos
+
+### Distribución del precio de las viviendas
+"""
+
+plt.figure()
+sns.histplot(df['SalePrice'], kde=True)
+plt.title('Distribución del Precio de las Casas')
+plt.show()
+
+"""### Correlación entre variables"""
+
+corr = df.corr(numeric_only=True)
+
+plt.figure()
+sns.heatmap(corr)
+plt.title('Mapa de correlación')
+plt.show()
+
+corr['SalePrice'].sort_values(ascending=False).head(10)
+
